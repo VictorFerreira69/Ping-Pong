34c1790a7b03b2aaa2dbc69acc43b6d02a9376e4
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2024-09-05 08:55:00 -0300

    Sistrema de Ui pronto

--- Ping Pong/Assets/Script/GameManager.cs
+++ Ping Pong/Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,10 +9,14 @@ public class GameManager : MonoBehaviour
 
     public int pontuaçaoDoJogador2;
 
+    public Text textoDePontuaçao;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pontuaçaoDoJogador1 = 0;
+        pontuaçaoDoJogador2 = 0;
+        textoDePontuaçao.text = pontuaçaoDoJogador1 + "X" + pontuaçaoDoJogador2;
     }
 
     // Update is called once per frame
@@ -22,9 +27,16 @@ public class GameManager : MonoBehaviour
    public  void AumentarPontuaçaoDoJogador1()
     {
         pontuaçaoDoJogador1 += 1;
+        AtulizarTextoDePontuaçao();
     }
    public void AumentarPontuaçaoDoJogador2()
     {
         pontuaçaoDoJogador2 += 1;
+        AtulizarTextoDePontuaçao();
+    }
+
+    public void AtulizarTextoDePontuaçao()
+    {
+        textoDePontuaçao.text = pontuaçaoDoJogador1 + "X" + pontuaçaoDoJogador2;
     }
 }
